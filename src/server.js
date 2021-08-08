04e4c27c70cd07c388a47f15a39bc4d699c6b5f4
pisckitama <pisckipratama@gmail.com>
2021-08-08 12:05:10 +0700

    restructred code

--- src/server.js
+++ src/server.js
@@ -1,22 +1,32 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const Hapi = require("@hapi/hapi");
+const notes = require("./api/notes");
+const NoteServices = require("./services/InMemory/noteServices");
 
 const init = async () => {
+  const noteServices = new NoteServices();
+
   const server = Hapi.server({
     port: 5000,
-    host: `${process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'}`,
+    host: `${process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost"}`,
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: noteServices,
+    },
+  });
 
   await server.start();
 
-  console.log(`server running in ${process.env.NODE_ENV} mode on ${server.info.uri}`);
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on ${server.info.uri}`
+  );
 };
 
 init();
