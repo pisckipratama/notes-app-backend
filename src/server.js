72719126cff9aa59977780e0281258e51d9d968e
pisckitama <pisckipratama@gmail.com>
2021-09-12 16:17:31 +0700

    add users service

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+
+// notes
 const notes = require("./api/notes");
 const NoteServices = require("./services/postgres/NoteService");
 const NotesValidator = require("./validator/notes");
 
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UserValidator = require("./validator/users");
+
 const init = async () => {
   const noteServices = new NoteServices();
+  const userServices = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -18,13 +26,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: noteServices,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: noteServices,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: userServices,
+        validator: UserValidator,
+      },
+    },
+  ]);
 
   await server.start();
 
